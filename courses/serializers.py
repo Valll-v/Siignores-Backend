09a0f1b9c25d8921318ed6931f6f80a22eca59f4
Valll-v <stepanenko.valera2013@gmail.com>
2022-08-27 16:56:55 +0300

    Modules

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework.serializers import ModelSerializer
 
-from courses.models import Course
+from courses.models import Course, Module
 
 
 class CourseSerializer(ModelSerializer):
     class Meta:
         model = Course
         fields = ('id', 'name', 'image', 'user')
+
+
+class GetModuleSerializer(ModelSerializer):
+    class Meta:
+        model = Module
+        fields = ('in_course_id', 'image', 'course')
+
+
+class PostModuleSerializer(ModelSerializer):
+    class Meta:
+        model = Module
+        fields = ('in_course_id', 'image', 'course')
